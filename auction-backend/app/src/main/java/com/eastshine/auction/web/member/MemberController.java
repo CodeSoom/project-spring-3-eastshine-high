009ec886f69c12b5c28f9a3f89d83a7acfc79fbe
eastshine-high <eastshine.high@gmail.com>
2021-11-04 21:39:53 +0900

    create member web layer

---
+++ auction-backend/app/src/main/java/com/eastshine/auction/web/member/MemberController.java
@@ -0,0 +1,24 @@
+package com.eastshine.auction.web.member;
+
+import com.eastshine.auction.application.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("members")
+public class MemberController {
+    private final MemberService memberService;
+
+    @PostMapping("")
+    public String signUpMember(@RequestBody @Valid MemberDto.Registration registration) {
+        Long memberId = memberService.signUpMember(registration);
+        return null;
+    }
+
+}
