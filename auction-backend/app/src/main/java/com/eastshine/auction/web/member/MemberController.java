4a673ab88945a6152841eb860092c642f985d577
eastshine-high <eastshine.high@gmail.com>
2021-11-05 20:30:21 +0900

    회원 API controller 일부 구현

--- auction-backend/app/src/main/java/com/eastshine/auction/web/member/MemberController.java
+++ auction-backend/app/src/main/java/com/eastshine/auction/web/member/MemberController.java
@@ -2,24 +2,47 @@ package com.eastshine.auction.web.member;
 
 import com.eastshine.auction.application.MemberService;
 import com.eastshine.auction.domain.member.Member;
+import com.github.dozermapper.core.Mapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.net.URI;
 
+/**
+ * 회원의 HTTP 요청에 대한 처리를 담당
+ */
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("members")
+@RequestMapping("/members")
 public class MemberController {
     private final MemberService memberService;
+    private final Mapper mapper;
 
-    @PostMapping("")
-    public String signUpMember(@RequestBody @Valid MemberDto.Signup signupRequest) {
-        Member member = memberService.signUpMember(signupRequest);
-        return null;
+    @PostMapping()
+    public ResponseEntity signUpMember(@RequestBody @Valid MemberDto.Signup request) {
+        Member requestSignup = mapper.map(request, Member.class);
+
+        Member signedUpMember = memberService.signUpMember(requestSignup);
+
+        return ResponseEntity.created(URI.create("/members/" + signedUpMember.getId())).build();
     }
 
+    @PatchMapping()
+    public ResponseEntity modifyMember(@RequestBody @Valid MemberDto.Modification request) {
+        Member requestMod = mapper.map(request, Member.class);
+
+        Member modifiedMember = memberService.modifyMember(requestMod);
+
+        return ResponseEntity.ok()
+                .location(URI.create("/members/" + modifiedMember.getId()))
+                .build();
+    }
 }
--- auction-backend/app/src/main/java/com/eastshine/auction/web/member/MemberDto.java
+++ auction-backend/app/src/main/java/com/eastshine/auction/web/member/MemberDto.java
@@ -3,14 +3,35 @@ package com.eastshine.auction.web.member;
 import com.github.dozermapper.core.Mapping;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 import javax.validation.constraints.NotBlank;
 
+
 public class MemberDto {
 
+    @ToString
     @Getter
     @Builder
     public static class Signup{
+
+        @Mapping("email")
+        @NotBlank
+        private String email;
+
+        @Mapping("password")
+        @NotBlank
+        private String password;
+    }
+
+    @ToString
+    @Getter
+    @Builder
+    public static class Modification{
+
+        @Mapping("id")
+        @NotBlank
+        private Long id;
 
         @Mapping("email")
         @NotBlank
